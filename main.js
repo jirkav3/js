7f287a69fb7c0e3a56600d300504eb68dca11e18
jiri.vanek <jiri.vanek@emtc.cz>
2017-06-22 00:25:51 +0200

    test

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 {(function(){
-console.log('banner');
+ console.log('banner');
+ var el='<div class="v-slot v-slot-announcementPanel v-slot-favorite_false"><img class="v-icon" src="/documents/11677/157053/Logo_aea02pi1baergs4f5l8hmr0mp8" alt="" style="width: 100%;"></div>';
+ $('.v-verticallayout.v-layout.v-vertical.v-widget.v-has-width').prepend(el);
 })();}
