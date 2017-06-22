dd94bd1d24e22c7f9e7f502beac1ba20f48f65b8
jiri.vanek <jiri.vanek@emtc.cz>
2017-06-22 13:55:36 +0200

    banner-aj.jpg

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = function () { {(function(){
  console.log('banner');
- var el='<div class="v-slot v-slot-announcementPanel v-slot-favorite_false"><img class="v-icon" src="/documents/11677/157053/Logo_aea02pi1baergs4f5l8hmr0mp8" alt="" style="width: 100%;"></div>';
+ var el='<div class="v-slot v-slot-announcementPanel v-slot-favorite_false"><img class="v-icon" src="http://i.bylipa.cz/content/other/airjobs/banner-aj.jpg" alt="" style="width: 100%;"></div>';
  if (typeof $ == 'function'){
     console.log('jquery is loaded');
     setTimeout(function(){
