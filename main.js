cd5ea5febcef01ee37d2e74d686b8c7b30456335
jiri.vanek <jiri.vanek@emtc.cz>
2017-06-22 13:20:27 +0200

    timeout

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ window.onload = function () { {(function(){
  var el='<div class="v-slot v-slot-announcementPanel v-slot-favorite_false"><img class="v-icon" src="/documents/11677/157053/Logo_aea02pi1baergs4f5l8hmr0mp8" alt="" style="width: 100%;"></div>';
  if (typeof $ == 'function'){
     console.log('jquery is loaded');
-    $('.v-verticallayout.v-layout.v-vertical.v-widget.v-has-width').prepend(el);   
+    setTimeout(function(){
+        $('.v-verticallayout.v-layout.v-vertical.v-widget.v-has-width').prepend(el);       
+    }, 5000);
+    
  }
 })();} }
